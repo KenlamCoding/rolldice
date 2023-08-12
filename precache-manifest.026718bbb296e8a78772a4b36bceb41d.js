13822b6ad3889fbd8e256764e5f02cb1e6a0a669
KenlamCoding <lky202355@gmail.com>
2023-08-12 18:13:21 +0800

    Updates

---
+++ precache-manifest.026718bbb296e8a78772a4b36bceb41d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "70164c8122cd722c2495",
+    "url": "/rolldice/static/css/main.6983285c.chunk.css"
+  },
+  {
+    "revision": "70164c8122cd722c2495",
+    "url": "/rolldice/static/js/main.70164c81.chunk.js"
+  },
+  {
+    "revision": "3e3c3eb55c09e01a1300",
+    "url": "/rolldice/static/js/1.3e3c3eb5.chunk.js"
+  },
+  {
+    "revision": "3c08d543a86a6a6a3f74",
+    "url": "/rolldice/static/js/2.3c08d543.chunk.js"
+  },
+  {
+    "revision": "63eafda9869d1c2352d4",
+    "url": "/rolldice/static/js/runtime~main.63eafda9.js"
+  },
+  {
+    "revision": "b75eb733e28d5687e5b0c79f2dc99cf7",
+    "url": "/rolldice/index.html"
+  }
+];
